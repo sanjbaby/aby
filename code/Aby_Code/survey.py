bae6e4d4727f445671b15575b84d4efd28eabc78
Sanju Baby <sanjbaby@in.ibm.com>
2020-03-22 12:55:40 +0530

    international survey

---
+++ code/Aby_Code/survey.py
@@ -0,0 +1,7 @@
+my_name = "Aby"
+my_age = "9 years old"
+your_name = input("What is your name? ")
+your_age = input("How old are you? ")
+print("My name is", my_name, ", and I am", my_age, "years old.")
+print(your_name, "what a nice name ! and ", your_name, "you are young !", "only", your_age, "years old")
+print("Thank you for taking this survey,", your_name, "!")
