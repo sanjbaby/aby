37823e64107404f0ebcf09bd38fd672cda1cf496
Sanju Baby <sanjbaby@in.ibm.com>
2020-03-22 12:19:26 +0530

    pizza hut

---
+++ code/Aby_Code/pizza hut.py
@@ -0,0 +1,54 @@
+print("Welcome to pizza hut sir/ma,am")
+number_of_pizzas = eval(input("How many pizzas would you like sir/ma'am ?"))
+cost_of_pizzas = eval(input("Can you read out the cost of 1 pizza sir/ma'am ?"))
+subtotal = number_of_pizzas * cost_of_pizzas
+tax = 0.8
+sales_tax = subtotal + tax
+total = subtotal * sales_tax
+print("The total cost is Rs", total)
+print("This includes Rs", subtotal, "and Rs", sales_tax, "for the sales tax")
+print("We hope you are satisfied with our care")
+print("BON APPETITE")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
